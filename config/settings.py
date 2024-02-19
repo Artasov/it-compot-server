bd47aa86097d55c0bea9fc9af3584b482dae4aa6
Artasov <ivanhvalevskey@gmail.com>
2024-02-19 18:04:35 +0300

    add lenk_shorter

--- config/settings.py
+++ config/settings.py
@@ -149,12 +149,6 @@ else:
             'PORT': env('SQL_PORT', '5432'),
         }
     }
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 CACHES = {
     'default': {
         "BACKEND": "django_redis.cache.RedisCache",
