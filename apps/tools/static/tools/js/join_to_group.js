be87392592a74ae41ba7068e2cb4ebc2d6b9a76d
Artasov <ivanhvalevskey@gmail.com>
2024-03-08 19:48:13 +0300

    process

--- apps/tools/static/tools/js/join_to_group.js
+++ apps/tools/static/tools/js/join_to_group.js
@@ -12,6 +12,10 @@ const confirmationModal = new bootstrap.Modal(document.getElementById('confirmat
 const nothingFitModal = new bootstrap.Modal(document.getElementById('nothingFitModal'), {
     keyboard: false
 });
+const chooseTZModal = new bootstrap.Modal(document.getElementById('chooseTZModal'), {
+    backdrop: 'static',
+    keyboard: false
+});
 
 function hideAllModals() {
     successModal.hide();
@@ -118,82 +122,58 @@ function joinStudentToGroup(student_id, group) {
     })
 }
 
-function createGroupEl(group) {
-    // Создаем элемент списка
+
+function createDateFromString(dateTimeString) {
+    // Пример входной строки '2024-03-23 19:40'
+    const parts = dateTimeString.split(' ');
+    const dateParts = parts[0].split('-');
+    const timeParts = parts[1].split(':');
+    return new Date(dateParts[0], dateParts[1] - 1, dateParts[2], timeParts[0], timeParts[1]);
+}
+
+function adjustDateHours(date, hoursChange) {
+    const newDate = new Date(date); // Создаем копию, чтобы не изменять исходный объект
+    newDate.setHours(newDate.getHours() + hoursChange);
+    return newDate;
+}
+
+function createGroupEl(group, TZ) {
     const li = document.createElement('li');
-    li.className = "unit-item rounded-3 text-light";
-    li.style.background = 'rgba(32,64,225,0.39)'
-    // Добавляем информацию о типе группы
-    // const typeP = document.createElement('p');
-    // typeP.textContent = unit.Type;
-    // li.appendChild(typeP);
+    li.className = "unit-item rounded-3 text-light bg-primary";
 
-
-
-
-    // Добавляем информацию о дате и времени
     const dateP = document.createElement('p');
     dateP.classList.add('fw-bold', 'fs-5', 'frsc', 'gap-2');
     const schedule = group.ScheduleItems[0];
-    dateP.innerHTML = `${schedule.BeginDate}
-        <span class="fw-normal fs-6" style="padding-bottom: 2px;">${schedule.BeginTime} - ${schedule.EndTime}</span>`;
+
+    // Вычисляем даты относительно TZ
+    const moscowTZ = 3; // Московское время +3
+    const diff = TZ - moscowTZ;
+    const datetimeStart = adjustDateHours(
+        createDateFromString(`${schedule.BeginDate} ${schedule.BeginTime}`),
+        diff
+    );
+    const datetimeEnd = adjustDateHours(
+        createDateFromString(`${schedule.BeginDate} ${schedule.EndTime}`),
+        diff
+    );
+    const day = datetimeStart.getDate().toString().padStart(2, '0');
+    const month = (datetimeStart.getMonth() + 1).toString().padStart(2, '0'); // месяцы начинаются с 0
+    const startHours = datetimeStart.getHours().toString().padStart(2, '0');
+    const startMinutes = datetimeStart.getMinutes().toString().padStart(2, '0');
+    const endHours = datetimeEnd.getHours().toString().padStart(2, '0');
+    const endMinutes = datetimeEnd.getMinutes().toString().padStart(2, '0');
+
+    dateP.innerHTML = `
+        <div class="frcc gap-2">
+            <span class="bg-primary-05 rounded-2 px-2" style="padding-top: 0.1em;">
+                ${day}.${month}
+            </span>
+            <span class="bg-primary-05 rounded-2 px-2 fw-normal fs-6" style="padding-top: 0.1em;">
+                ${startHours}:${startMinutes} - ${endHours}:${endMinutes}
+            </span>
+        </div>
+    `;
     li.appendChild(dateP);
-
-
-
-
-
-
-
-
-
-
-
-
-    // // Добавляем информацию о дате и времени
-    // const dateP = document.createElement('div');
-    // dateP.classList.add('fw-bold', 'fs-5', 'frsc', 'gap-2', 'mb-3');
-    //
-    // // Генерируем время в соответствии с TZ пользователя.
-    // const schedule = group.ScheduleItems[0];
-    // const userTimezoneOffset = new Date().getTimezoneOffset() * -1;
-    // const scheduleTimezoneOffset = 180; // Таймзона +3 в минутах (данные приходят по МСК)
-    // const timezoneDifference = userTimezoneOffset - scheduleTimezoneOffset;
-    //
-    // function adjustTime(time, difference) {
-    //     const [hours, minutes] = time.split(':').map(Number);
-    //     const date = new Date();
-    //     date.setHours(hours, minutes + difference, 0, 0);
-    //     return date.toTimeString().slice(0, 5);
-    // }
-    //
-    // const adjustedBeginTime = adjustTime(schedule.BeginTime, timezoneDifference);
-    // const adjustedEndTime = adjustTime(schedule.EndTime, timezoneDifference);
-    //
-    // // Форматирование даты для отображения только дня и месяца
-    // const displayDate = new Date(schedule.BeginDate).toLocaleDateString('ru-RU', {
-    //     day: '2-digit',
-    //     month: '2-digit',
-    // });
-    //
-    // dateP.innerHTML = `
-    // <span class="rounded-2 px-2" style="padding-top: 2px;background-color: rgba(32,64,225,0.2)">
-    //     ${displayDate}
-    // </span>
-    // <div class="fcc position-relative">
-    //     <span class="fw-normal fs-6 p-0" style="padding-bottom: 2px;">
-    //         <span class="fw-bold py-0 rounded-2 px-2" style="background-color: rgba(32,64,225,0.2);">
-    //             ${adjustedBeginTime} - ${adjustedEndTime}
-    //         </span>&nbsp;&nbsp;Ваше время
-    //     </span>
-    //     ${timezoneDifference !== 0 ? `
-    //     <span class="fw-normal p-0 position-absolute"
-    //           style="padding-bottom: 2px; margin-left: 0.37rem; font-size: 0.6em; bottom: -1.1rem;">
-    //         ${schedule.BeginTime} - ${schedule.EndTime} МСК
-    //     </span>` : ''}
-    // </div>
-    // `;
-    // li.appendChild(dateP);
 
     // Добавляем информацию о количестве учеников и вакансиях
     const frbDiv = document.createElement('div');
@@ -234,6 +214,52 @@ async function send_nothing_fit(student_id, msg) {
 }
 
 
+let loadedGroups = undefined;
+
+const resultContainerEl = document.querySelector('.result_container');
+const groupLoadingStatusContainerEl = document.querySelector('.group_loading_status_container');
+
+function showGroupsWithTZ(TZ) {
+    // TZ - цифра (данные в группах указаны по умолчанию в +3)
+    // например -1 +1 +3 +2 и тому подобные цифры.
+    // Могут быть как отрицательные, так и положительные.
+    for (const group of loadedGroups) {
+        const groupEl = createGroupEl(group, TZ)
+        groupEl.addEventListener('click', () => {
+            const studentId = Client.getParamsFromCurrentURL()['student_id'];
+            confirmationModal.show();
+
+            const modalBodyEl = document.querySelector('.confirmation-modal-body');
+            modalBodyEl.innerHTML = '';
+            const groupPreview = createGroupEl(group, TZ);
+            groupPreview.classList.add('pointer-events-none', 'fs-5');
+            const submitQuestionText = document.createElement('p');
+            submitQuestionText.className = 'text-center mb-0 mt-2 fs-5';
+            submitQuestionText.innerHTML = 'Вы действительно хотите записаться в выбранную группу?';
+            modalBodyEl.appendChild(groupPreview);
+            modalBodyEl.appendChild(submitQuestionText);
+
+
+            document.getElementById('confirmJoin').onclick = () => {
+                document.getElementById('confirmSpinner').classList.remove('d-none')
+                joinStudentToGroup(studentId, group);
+                document.getElementById('confirmJoin').setAttribute('disabled', 'true');
+            };
+        });
+        resultContainerEl.appendChild(groupEl);
+    }
+    document.getElementById('btn-nothing-fit').classList.remove('d-none');
+    resultContainerEl.parentElement.classList.remove('d-none');
+    groupLoadingStatusContainerEl.innerHTML = '';
+    groupLoadingStatusContainerEl.className = '';
+    const successEl = document.createElement('p');
+    successEl.className = 'text-center fw-bold fs-5 m-0 opacity-75';
+    successEl.style.color = '#4058ff'
+    successEl.innerHTML = 'Готово! Найденные группы ниже.';
+    groupLoadingStatusContainerEl.appendChild(successEl);
+}
+
+
 async function main() {
     // Если параметров в запросе достаточно
     if (Object.keys(Client.getParamsFromCurrentURL()).length !== 4) {
@@ -268,9 +294,6 @@ async function main() {
     } else if (alreadyStudying === null) return;
 
     // Подгружаем группы
-    const resultContainerEl = document.querySelector('.result_container');
-    const groupLoadingStatusContainerEl = document.querySelector('.group_loading_status_container');
-
     getAvailableFormingGroups().then(groups => {
         if (groups.length === 0) {
             document.getElementById('nothing_fit_user_textarea').setAttribute(
@@ -282,45 +305,19 @@ async function main() {
             return;
 
         }
-        for (const group of groups) {
-            const groupEl = createGroupEl(group)
-
-            // Измените обработчик событий для groupEl
-            groupEl.addEventListener('click', () => {
-                const studentId = Client.getParamsFromCurrentURL()['student_id'];
-                confirmationModal.show();
-
-                const modalBodyEl = document.querySelector('.confirmation-modal-body');
-                modalBodyEl.innerHTML = '';
-                const groupPreview = createGroupEl(group);
-                groupPreview.classList.add('pointer-events-none', 'fs-5');
-                const submitQuestionText = document.createElement('p');
-                submitQuestionText.className = 'text-center mb-0 mt-2 fs-5';
-                submitQuestionText.innerHTML = 'Вы действительно хотите записаться в выбранную группу?';
-                modalBodyEl.appendChild(groupPreview);
-                modalBodyEl.appendChild(submitQuestionText);
-
-
-                document.getElementById('confirmJoin').onclick = () => {
-                    document.getElementById('confirmSpinner').classList.remove('d-none')
-                    joinStudentToGroup(studentId, group);
-                    document.getElementById('confirmJoin').setAttribute('disabled', 'true');
-                };
-            });
-
-
-            resultContainerEl.appendChild(groupEl);
+        loadedGroups = groups
+        const btnsChooseTz = document.getElementsByClassName('btn-choose-tz');
+        for (const btnTz of btnsChooseTz) {
+            btnTz.addEventListener('click', () => {
+                chooseTZModal.hide();
+                document.getElementById('tz-info').classList.remove('d-none');
+                document.getElementById('tz-span').innerHTML = btnTz.value;
+                showGroupsWithTZ(parseInt(btnTz.value));
+            })
         }
-        document.getElementById('btn-nothing-fit').classList.remove('d-none');
-        resultContainerEl.parentElement.classList.remove('d-none');
-        groupLoadingStatusContainerEl.innerHTML = '';
-        groupLoadingStatusContainerEl.className = '';
-        const successEl = document.createElement('p');
-        successEl.className = 'text-center fw-bold fs-5 m-0 opacity-75';
-        successEl.style.color = '#4058ff'
-        successEl.innerHTML = 'Готово! Найденные группы ниже.';
-        groupLoadingStatusContainerEl.appendChild(successEl);
+        chooseTZModal.show();
     });
+
 }
 
 await main();
