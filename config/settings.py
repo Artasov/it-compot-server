76e6cff4a1a4e45d6184f6a9bb43901eeee5cdbe
Artasov <ivanhvalevskey@gmail.com>
2024-06-26 14:12:31 +0300

    process

--- config/settings.py
+++ config/settings.py
@@ -170,7 +170,7 @@ else:
             'ENGINE': env('SQL_ENGINE'),
             'NAME': env('POSTGRES_DB'),
             'USER': env('POSTGRES_USER'),
-            'PASSWORD': env('SQL_PASSWORD'),
+            'PASSWORD': env('POSTGRES_PASSWORD'),
             'HOST': env('SQL_HOST'),
             'PORT': env('POSTGRES_PORT'),
         }
