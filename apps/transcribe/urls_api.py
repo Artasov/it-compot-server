c835f48250901680d8eadc4d11aeef4b02148ea3
Artasov <ivanhvalevskey@gmail.com>
2024-07-10 06:45:23 +0300

    process

--- apps/transcribe/urls_api.py
+++ apps/transcribe/urls_api.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from transcribe.api import transcribe_lead_call
+from apps.transcribe.api import transcribe_lead_call
 
 urlpatterns = [
     path('transcribe_lead_call/', transcribe_lead_call, name='transcribe_lead_call'),
