10283773b48f93934c8e4ebc121b291a9e55b9bb
Artasov <ivanhvalevskey@gmail.com>
2024-07-19 03:15:12 +0300

    process

--- apps/tools/services/lesson_report/funcs.py
+++ apps/tools/services/lesson_report/funcs.py
@@ -5,8 +5,9 @@ from typing import TypedDict
 
 from django.conf import settings
 
-from modules.hollihop.classes.custom_hollihop import CustomHHApiV2Manager
 from modules.gsheet.classes.gsheetsclient import GSDocument, GSFormatOptions
+from modules.hollihop.classes.custom_hollihop import CustomHHApiV2Manager, SetCommentError
+from modules.pickler import PicklerNotFoundDumpFile
 
 
 class LessonComment(TypedDict):
@@ -14,6 +15,53 @@ class LessonComment(TypedDict):
     theme: str
     finish_percent: int | None
     add_info: str
+
+
+async def send_lesson_report(
+        ed_unit_id,
+        day_date,
+        theme_number,
+        theme_name,
+        lesson_completion_percentage,
+        students_comments,
+        type_ed_unit,
+        user_email,
+        username,
+):
+    from apps.tools.api import pickler
+    HHManager = CustomHHApiV2Manager()
+    try:
+        teacher_name = pickler.cache(f'{username}_full_teacher_name')
+    except PicklerNotFoundDumpFile:
+        teacher_name = await HHManager.get_full_teacher_name_by_email(user_email)
+        pickler.cache(f'{username}_full_teacher_name', teacher_name, 24 * 60 * 60 * 4)
+
+    for student_comment in students_comments:
+        try:
+            await HHManager.set_comment_for_student_ed_unit(
+                ed_unit_id=ed_unit_id,
+                student_client_id=student_comment['ClientId'],
+                date=day_date,
+                passed=student_comment['Pass'],
+                description=f'* {theme_number}{". " if theme_number else ""}{theme_name}\n'
+                            f'* Завершено на: {lesson_completion_percentage}%\n'
+                            f'* {student_comment["Description"]}'
+            )
+            await send_gs_lesson_report(
+                teacher_name=teacher_name,
+                type_ed_unit=type_ed_unit,
+                ed_unit_id=ed_unit_id,
+                student_name=student_comment['StudentName'],
+                student_amo_id=student_comment['StudentAmoId'],
+                student_client_id=student_comment['ClientId'],
+                date=day_date,
+                description=f'* {theme_number}. {theme_name}\n'
+                            f'* Завершено на: {lesson_completion_percentage}%\n'
+                            f'* {student_comment["Description"]}'
+            )
+        except SetCommentError:
+            return {'success': False, 'error': 'Ошибка при добавлении комментария в HH'}
+    pickler.delete(f'{username}_lessons')
 
 
 def parse_lesson_comment(comment: str) -> LessonComment | None:
