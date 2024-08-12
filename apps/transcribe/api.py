13973ff6c685ae62cc1c83542f637e7b62e845c8
Artasov <ivanhvalevskey@gmail.com>
2024-08-12 16:23:10 +0300

    process

--- apps/transcribe/api.py
+++ apps/transcribe/api.py
@@ -9,7 +9,7 @@ from apps.Core.services.base import acontroller
 from apps.transcribe.serializers import AmoCallTranscribeSerializer
 from modules.common.common import download_file
 from modules.gpt.classes.whisper import Whisper
-from modules.gsheet.classes.gsheetsclient import GSDocument, GSFormatOptionVariant
+from modules.gsheet.classes.gsheetsclient import GSDocument
 from modules.pickler import Pickler
 
 log = logging.getLogger('base')
@@ -31,27 +31,18 @@ async def transcribe_lead_call(request):
         print('RECOGNITION')
         print(transcript)
         os.remove(temp_file_path)
-        GSDocument(settings.GSDOCID_UPLOAD_BY_LESSON).update_sheet_with_format_header(
-            sheet_name='Wrong Comments',
-            header=(
-                'LeadId',
-                'ContactId',
-                'AudioUrl',
-                'Responsible',
-                'Duration',
-                'Status',
-                'Reason',
-            ),
-            values=[(
+        GSDocument(settings.GSDOCID_TRANSCRIBE_LEAD_CALL).append_row(
+            sheet_name='All',
+            row=(
                 f'https://itbestonlineschool.amocrm.ru/leads/detail/{data["lead_id"]}',
                 f'https://itbestonlineschool.amocrm.ru/contacts/detail/{data["contact_id"]}',
+                transcript,
                 data['call_audio_url'],
                 data.get('responsible'),
                 data.get('duration'),
                 data.get('status'),
                 data.get('reason'),
-            )],
-            format_header=GSFormatOptionVariant.BASE_HEADER
+            ),
         )
         return Response({'success': True}, 200)
     else:
--- modules/common/common.py
+++ modules/common/common.py
@@ -52,12 +52,8 @@ def create_virtual_csv(data) -> StringIO:
 
 
 async def download_file(url: str) -> str:
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-    }
-
-    async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(url) as response:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers={"User-Agent": "Mozilla/5.0", "Accept": "*/*"}) as response:
             if response.status == 200:
                 content_disposition = response.headers.get('Content-Disposition')
                 if content_disposition and 'filename=' in content_disposition:
@@ -67,7 +63,8 @@ async def download_file(url: str) -> str:
                     file_name = os.path.basename(parsed_url.path)
                 file_path = settings.BASE_TEMP_DIR / file_name
                 file_path.parent.mkdir(parents=True, exist_ok=True)
-                if file_path.exists(): return str(file_path)
+                if file_path.exists():
+                    return str(file_path)
                 with open(file_path, 'wb') as f:
                     f.write(await response.read())
             else:
